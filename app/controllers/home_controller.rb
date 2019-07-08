f31a670776633b9b84e21cc6595b6d233d80a329
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-08 17:39:39 +0200

    add comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@
 
 class HomeController < ApplicationController
   def index
+    # ActiveRecord::Base.includes preloads all variants
+    # This is called Eager Loading and aims to avoid n+1 queries
     @authors = Author.all.includes(:books)
   end
 end
