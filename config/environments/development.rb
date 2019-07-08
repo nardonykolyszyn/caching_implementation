b8d867d709b3bc0899844b39d7a1b1261caae59f
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-09 00:39:16 +0200

    add initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,12 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+  end
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
