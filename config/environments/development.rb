48888afddb2e2c93f8bba7e30a3965216004eb33
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-09 00:15:53 +0200

    enable caching in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
   end
