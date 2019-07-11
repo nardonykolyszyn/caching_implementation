07b888bbe54e8273457ef4de5874c66d6e3b1ce0
Nardo Nykołyszyn <nardonykolyszyn@gmail.com>
2019-07-11 19:15:05 +0200

    add top-level documentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# How it works?
+# If we only call for the authors without their children collections (Books)
+# Once we iterate over this instance variable in the views to get all the books of each author
+# We are going to perform this operation: SELECT * FROM "public"."books" WHERE "book"."author_id" = $(AUTHOR_ID)
+# So, let's imagine we have 100 authors and we need to get all their books, this would be a huge problem
+# Since we are going to call the same query for those 100 authors, we'll have (100) + 1 queries
+# Eager loading automatically includes all books for all authors by only using an extra query
+# This approach aims to have only two queries for N authors instead of N+1
+
 class HomeController < ApplicationController
   def index
     # ActiveRecord::Base.includes preloads all variants
